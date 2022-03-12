73a41e714ac1439be3cdcdf18f45c01dee4818b5
Acoleman37 <aleccoleman123@yahoo.com>
2022-03-11 18:50:57 -0800

    file structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
-function App() {
+export default function App() {
   return (
-    <div className="flex-column justify-flex-start min-100-vh">
-      <Header />
-      <div className="container">
-      </div>
-      <Footer />
+    <div>
+      <main className="">
+        <Navbar />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
     </div>
   );
 }
-
-export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: true,
+  theme: {
+    colors: {
+      'blue': '#0284c7'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
